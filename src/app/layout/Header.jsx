7e74adfcb00602cf995ecaa98cf7c86cd32bb759
spde3289 <spde3289@naver.com>
2025-10-28 16:37:58 +0900

    feat: 헤더 nav 바 추가, 유저 이미지 추가

--- src/app/layout/Header.jsx
+++ src/app/layout/Header.jsx
@@ -1,24 +1,61 @@
+import user from "@/assets/user.png";
 import { Link } from "react-router-dom";
-import smLogo from "../../assets/smLogo.png";
 
 import { PATH } from "../router";
 import * as S from "./Header.styles";
 
+const HEADER_DATA = {
+  logo: {
+    href: PATH.INDEX,
+    ariaLabel: "판다마켓 홈",
+  },
+  navLinks: [
+    { name: "자유 게시판", href: "/" },
+    { name: "중고마켓", href: PATH.ITEMS },
+  ],
+  userImg: {
+    imageSrc: user,
+    imageAlt: "유저 이미지",
+  },
+  loginButton: {
+    text: "로그인",
+    href: PATH.LOGIN,
+  },
+};
+
 const Header = () => {
+  const { logo, userImg, navLinks, loginButton } = HEADER_DATA;
+
+  const isLogin = true;
+
   return (
     <S.Header>
       <S.HeaderInner>
-        <S.Logo>
-          <Link to={PATH.INDEX} rel="home" aria-label="판다마켓 홈">
-            <img src={smLogo} alt="판다마켓 로고" />
+        <S.Nav>
+          <Link to={logo.href} rel="home">
+            <S.Logo></S.Logo>
           </Link>
-        </S.Logo>
-        <S.LoginButton as={Link} to={PATH.LOGIN}>
-          로그인
-        </S.LoginButton>
+          <S.Ul>
+            {navLinks.map((item) => (
+              <li key={item.name}>
+                <Link to={item.href}>{item.name}</Link>
+              </li>
+            ))}
+          </S.Ul>
+        </S.Nav>
+        {isLogin ? (
+          <S.UserImg
+            src={userImg.imageSrc}
+            alt={userImg.imageAlt}
+            tabIndex={0}
+          />
+        ) : (
+          <S.LoginButton as={Link} to={loginButton.href}>
+            {loginButton.text}
+          </S.LoginButton>
+        )}
       </S.HeaderInner>
     </S.Header>
   );
 };
-
 export default Header;
