282d94eca18ac7a8133524e5f4d6a5dbb90b2095
spde3289 <spde3289@naver.com>
2025-11-27 20:25:00 +0900

    refact: 라우팅 NavLink 를 이용한 하이라이트 구현

--- src/app/layout/Header.jsx
+++ src/app/layout/Header.jsx
@@ -1,68 +1,51 @@
 import user from "@/assets/user.png";
-import { Link, useLocation } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
 import { PATH } from "../router";
 import * as S from "./Header.styles";
 
-const HEADER_DATA = {
-  logo: {
-    href: PATH.INDEX,
-    ariaLabel: "판다마켓 홈",
-  },
-  navLinks: [
-    { name: "자유 게시판", href: "/" },
-    { name: "중고마켓", href: PATH.ITEMS },
-  ],
-  userImg: {
-    imageSrc: user,
-    imageAlt: "유저 이미지",
-  },
-  loginButton: {
-    text: "로그인",
-    href: PATH.LOGIN,
-  },
-};
-
 const Header = () => {
-  const location = useLocation();
-  const { logo, userImg, navLinks, loginButton } = HEADER_DATA;
   const isLogin = true;
 
   return (
     <S.Header>
       <S.HeaderInner>
         <S.Nav>
-          <Link to={logo.href} rel="home">
-            <S.Logo></S.Logo>
+          <Link to={PATH.INDEX} rel="home">
+            <S.Logo />
           </Link>
           <S.Ul>
-            {navLinks.map((item) => (
-              <li
-                style={
-                  location.pathname === item.href
-                    ? { color: "var(--blue100)" }
-                    : {}
+            <li>
+              <NavLink
+                to="/"
+                style={({ isActive }) =>
+                  isActive ? { color: "var(--blue100)" } : undefined
                 }
-                key={item.name}
               >
-                <Link to={item.href}>{item.name}</Link>
-              </li>
-            ))}
+                자유 게시판
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={PATH.ITEMS}
+                style={({ isActive }) =>
+                  isActive ? { color: "var(--blue100)" } : undefined
+                }
+              >
+                중고마켓
+              </NavLink>
+            </li>
           </S.Ul>
         </S.Nav>
         {isLogin ? (
-          <S.UserImg
-            src={userImg.imageSrc}
-            alt={userImg.imageAlt}
-            tabIndex={0}
-          />
+          <S.UserImg src={user} alt="유저 이미지" />
         ) : (
-          <S.LoginButton as={Link} to={loginButton.href}>
-            {loginButton.text}
+          <S.LoginButton as={Link} to={PATH.LOGIN}>
+            로그인
           </S.LoginButton>
         )}
       </S.HeaderInner>
     </S.Header>
   );
 };
+
 export default Header;
