b2028c77a8f8970a1f391f67b3d0639a0bd57248
spde3289 <spde3289@naver.com>
2025-12-02 16:38:26 +0900

    fix: 헤더 하이라이트

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -31,8 +31,10 @@ const Header = () => {
             <li>
               <NavLink
                 to={PATH.ITEMS}
-                style={() =>
-                  isMarketActive ? { color: "var(--blue100)" } : undefined
+                style={({ isActive }) =>
+                  isMarketActive || isActive
+                    ? { color: "var(--blue100)" }
+                    : undefined
                 }
               >
                 중고마켓
