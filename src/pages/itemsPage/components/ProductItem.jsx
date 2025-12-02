0d7fff7b08bdb8748f34da563861036514009637
spde3289 <spde3289@naver.com>
2025-12-02 17:22:52 +0900

    feat: 아이템 상세페이지 link

--- src/pages/itemsPage/components/ProductItem.jsx
+++ src/pages/itemsPage/components/ProductItem.jsx
@@ -1,27 +1,30 @@
 import icHeart from "@/assets/imgs/ic_heart.png";
 import defaultImage from "@/assets/imgs/panda-market.png";
+import { Link } from "react-router-dom";
 import * as S from "./ProductItem.styles";
 
 const ProductItem = ({ item, size = "md" }) => {
   return (
-    <S.Container>
-      <S.ProductImgBox size={size}>
-        <S.ProductImg
-          src={item.images}
-          onError={(e) => {
-            e.currentTarget.src = defaultImage;
-          }}
-        />
-      </S.ProductImgBox>
-      <S.ProductDescription>
-        <S.ProductTitle>{item.name}</S.ProductTitle>
-        <S.ProductPrice>{item.price.toLocaleString()}원</S.ProductPrice>
-        <S.ProductFavoriteCount>
-          <img src={icHeart} alt="좋아요" />
-          {item.favoriteCount}
-        </S.ProductFavoriteCount>
-      </S.ProductDescription>
-    </S.Container>
+    <Link to={`${item.id}`}>
+      <S.Container>
+        <S.ProductImgBox size={size}>
+          <S.ProductImg
+            src={item.images}
+            onError={(e) => {
+              e.currentTarget.src = defaultImage;
+            }}
+          />
+        </S.ProductImgBox>
+        <S.ProductDescription>
+          <S.ProductTitle>{item.name}</S.ProductTitle>
+          <S.ProductPrice>{item.price.toLocaleString()}원</S.ProductPrice>
+          <S.ProductFavoriteCount>
+            <img src={icHeart} alt="좋아요" />
+            {item.favoriteCount}
+          </S.ProductFavoriteCount>
+        </S.ProductDescription>
+      </S.Container>
+    </Link>
   );
 };
 
