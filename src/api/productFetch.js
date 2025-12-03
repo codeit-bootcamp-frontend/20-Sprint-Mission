f40699d13957f542ca60b9b88d7d030886971613
spde3289 <spde3289@naver.com>
2025-12-03 17:03:23 +0900

    chore: 함수 폴더 정리

--- src/api/productFetch.js
+++ src/api/productFetch.js
@@ -8,3 +8,18 @@ export const getProductDetail = async (productId) => {
 
   return data;
 };
+
+/**
+ * @param { number } productId // 아이템 ID
+ * @param {{
+ *   limit: number,
+ *   cursor?: number,
+ * }} params
+ */
+export const getProductCommentList = async (productId, params) => {
+  const data = await api.get(`${ENDPOINTS.products}/${productId}/comments`, {
+    params,
+  });
+
+  return data;
+};
